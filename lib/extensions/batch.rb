6b4fd2e13ed494f79941bb3b99bbd618366fd62c
Georgi Dankov <gddankov@gmail.com>
2025-06-06 14:23:05 +0100

    Add extension for Async client

--- lib/extensions/batch.rb
+++ lib/extensions/batch.rb
@@ -17,5 +17,29 @@ module Paid
       end
     end
   end
+
+
+  class AsyncBatchUsageClient < Paid::AsyncUsageClient
+    def flush(request_options: nil)
+      Async do
+        record_bulk(signals: @signals, request_options: request_options)
+        @signals = []
+      end
+    end
+
+    def record_usage(signal: nil, request_options: nil)
+      Async do
+        @signals ||= []
+        @signals << signal
+        if @signals.length >= 100
+            record_bulk(signals: @signals, request_options: request_options)
+            @signals = []
+        end
+      end
+    end
+  end
+
   UsageClient = BatchUsageClient
+  AsyncUsageClient = AsyncBatchUsageClient
+
 end
